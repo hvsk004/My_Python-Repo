0b627dde73f8161f3f7447a813c56bc44761a498
Himanth Venkata Sai Krishna <himanth2004@gmail.com>
2022-10-01 16:14:59 +0530

    Adding swapping program

---
+++ Swapping Program.py
@@ -0,0 +1,7 @@
+variable1=input("Enter 1st Number")
+variable2=input("Enter 2nd numebr")
+temp=variable2
+variable2=variable1
+variable1=temp
+print("The First numebr is :",variable1)
+print("The Second Number is :",variable2)
