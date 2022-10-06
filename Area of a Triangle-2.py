64839161fc6c75ff403f98aa93fe976dc9b80467
Himanth Venkata Sai Krishna <himanth2004@gmail.com>
2022-10-06 22:02:32 +0530

    Adding programs

---
+++ Area of a Triangle-2.py
@@ -0,0 +1,6 @@
+a=input("Enter the length of side A ")
+b=input("Enter the Length of side B ")
+c=input("Enter Tthe length of side C ")
+s=(int(a)+int(b)+int(c))/2
+A=sqrt(int(s)*(int(s)-int(a))*(int(s)-int(b))*(int(s)-int(c)))
+print("The Area of the triangle is",A)
---
+++ compare number.py
@@ -0,0 +1,6 @@
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+if int(a)>int(b):
+ print("a is greater")
+else:
+ print("b is greater")
