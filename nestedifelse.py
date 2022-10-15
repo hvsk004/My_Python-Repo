9e2d9dc2aa010589e61c67efcc65e8f149b9963b
Himanth Venkata Sai Krishna <himanth2004@gmail.com>
2022-10-15 12:55:00 +0530

    Commiting nested if program

---
+++ nestedifelse.py
@@ -0,0 +1,12 @@
+x=int(input("Enter a number"))
+if x<200:
+    print("Variable is less than 200")
+    if x<150:
+        print("Variable is less than 150")
+    elif x==150:
+        print("Variable is 150")
+    elif x<10:
+        print("Variabke is less than 10")
+else:
+    print("Error 404")
+print("Hello World")
