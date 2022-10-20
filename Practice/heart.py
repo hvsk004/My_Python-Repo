f8dcf656133bbabaf2ccd039063cbe1f01dffeba
Himanth Venkata Sai Krishna <himanth2004@gmail.com>
2022-10-20 14:57:05 +0530

    updating heart program

--- Practice/heart.py
+++ Practice/heart.py
@@ -1,11 +1,70 @@
-from turtle import*
-color("red")
-begin_fill()
-pensize(3)
-left(50)
-forward(133)
-circle(50,200)
-right(140)
-circle(50,200)
-forward(133)
-end_fill()
+# Import turtle package
+import turtle
+
+# Creating a turtle object(pen)
+pen = turtle.Turtle()
+
+# Defining a method to draw curve
+def curve():
+	for i in range(200):
+
+		# Defining step by step curve motion
+		pen.right(1)
+		pen.forward(1)
+
+# Defining method to draw a full heart
+def heart():
+
+	# Set the fill color to red
+	pen.fillcolor('red')
+
+	# Start filling the color
+	pen.begin_fill()
+
+	# Draw the left line
+	pen.left(140)
+	pen.forward(113)
+
+	# Draw the left curve
+	curve()
+	pen.left(120)
+
+	# Draw the right curve
+	curve()
+
+	# Draw the right line
+	pen.forward(112)
+
+	# Ending the filling of the color
+	pen.end_fill()
+
+# Defining method to write text
+def txt():
+
+	# Move turtle to air
+	pen.up()
+
+	# Move turtle to a given position
+	pen.setpos(-68, 95)
+
+	# Move the turtle to the ground
+	pen.down()
+
+	# Set the text color to lightgreen
+	pen.color('lightgreen')
+
+	# Write the specified text in
+	# specified font style and size
+	pen.write("GeeksForGeeks", font=(
+	"Verdana", 12, "bold"))
+
+
+# Draw a heart
+heart()
+
+# Write text
+txt()
+
+# To hide turtle
+pen.ht()
+turtle.Screen().exitonclick()
